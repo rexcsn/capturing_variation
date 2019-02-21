79bf4ad38e9182e934c3a583adf7c0b67fcc91a8
Rex Chen <rexcsn@umich.edu>
2019-02-21 14:18:53 -0500

    exception

--- src/data_to_corpus.py
+++ src/data_to_corpus.py
@@ -10,8 +10,8 @@ import bz2
 import string
 import re
 
-project_path = "/shared/0/projects/location-inference/working-dir/textual_data"
-# project_path = "."
+# project_path = "/shared/0/projects/location-inference/working-dir/textual_data"
+project_path = "."
 
 
 def extract_info(filename):
@@ -74,9 +74,9 @@ def to_corpus(df):
 
 
 def main():
-    df = extract_info("/twitter-turbo/decahose/raw/decahose.2019-02-02.p1.bz2")
+    # df = extract_info("/twitter-turbo/decahose/raw/decahose.2019-02-02.p1.bz2")
     # df = extract_info("./sample_text.txt.bz2")
-    # df = extract_info("./baby.txt.bz2")
+    df = extract_info("./baby.txt.bz2")
     to_corpus(df)
 
 
