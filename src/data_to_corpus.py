c2f94ceb606c5e81e3547b2e8e1353351f4b605e
Rex Chen <rexcsn@umich.edu>
2019-02-21 14:32:26 -0500

    exception

--- src/data_to_corpus.py
+++ src/data_to_corpus.py
@@ -15,36 +15,32 @@ project_path = "."
 
 
 def extract_info(filename):
-    try:
-        file = bz2.BZ2File(filename)
-        collect = []
-        for line in file:
-            js = json.loads(line)
-            collect.append(js)
+    file = bz2.BZ2File(filename)
+    collect = []
+    for line in file:
+        js = json.loads(line)
+        collect.append(js)
 
-        df = pd.DataFrame(collect)
-        df = df[['user', 'text', 'id', 'source', 'place', 'geo']]
+    df = pd.DataFrame(collect)
+    df = df[['user', 'text', 'id', 'source', 'place', 'geo']]
 
-        df = df[df['geo'].notnull()]
+    df = df[df['geo'].notnull()]
 
-        filtered = pd.DataFrame()
+    filtered = pd.DataFrame()
 
-        filtered = filtered.assign(user_id=df['user'].apply(pd.Series)['id'],
-                                   tweet_id=df['id'],
-                                   text=df['text'],
-                                   lat=df['geo'].apply(pd.Series)['coordinates'][0][0],
-                                   lon=df['geo'].apply(pd.Series)['coordinates'][0][1],
-                                   city=df['place'].apply(pd.Series)['full_name'],
-                                   country_code=df['place'].apply(pd.Series)['country_code'],
-                                   source=df['source']
-                                   )
+    filtered = filtered.assign(user_id=df['user'].apply(pd.Series)['id'],
+                               tweet_id=df['id'],
+                               text=df['text'],
+                               lat=df['geo'].apply(pd.Series)['coordinates'][0][0],
+                               lon=df['geo'].apply(pd.Series)['coordinates'][0][1],
+                               city=df['place'].apply(pd.Series)['full_name'],
+                               country_code=df['place'].apply(pd.Series)['country_code'],
+                               source=df['source']
+                               )
 
-        filtered.to_csv(
-            "%s/extracted_2019-02-02" % project_path)
-        print("Saved df to %s/extracted_2019-02-02 !!!" % project_path)
-    except Exception as e:
-        print(e)
-        pass
+    filtered.to_csv(
+        "%s/extracted_2019-02-02" % project_path)
+    print("Saved df to %s/extracted_2019-02-02 !!!" % project_path)
 
     return filtered
 
@@ -57,18 +53,14 @@ def text_to_words(df):
 
 
 def to_corpus(df):
-    try:
-        dict_df = pd.DataFrame()
-        dict_df = dict_df.assign(tags=(df['city']+","+df['country_code']).str.replace(' ', ''),
-                                 words=df['text'].str.split(r'\W+'))
+    dict_df = pd.DataFrame()
+    dict_df = dict_df.assign(tags=(df['city']+","+df['country_code']).str.replace(' ', ''),
+                             words=df['text'].str.split(r'\W+'))
 
-        dict_df.to_json(
-            "%s/sample_corpus_2019-02-02" % project_path,
-            orient='records')
-        print("Saved corpus to %s/sample_corpus_2019-02-02 !!!" % project_path)
-    except Exception as e:
-        print(e)
-        pass
+    dict_df.to_json(
+        "%s/sample_corpus_2019-02-02" % project_path,
+        orient='records')
+    print("Saved corpus to %s/sample_corpus_2019-02-02 !!!" % project_path)
 
     return dict_df
 
