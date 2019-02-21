84d0d68120f352cbb49d39590155c024dea2066d
Rex Chen <rexcsn@umich.edu>
2019-02-21 11:10:51 -0500

    test

--- src/data_to_corpus.py
+++ src/data_to_corpus.py
@@ -57,7 +57,7 @@ def text_to_words(df):
 def to_corpus(df):
     dict_df = pd.DataFrame()
     dict_df = dict_df.assign(tags=(df['city']+","+df['country_code']).str.replace(' ', ''),
-                             words=re.split('\W+', df['text'].str))
+                             words=df['text'].str.split(r'\W+'))
 
     dict_df.to_json(
         "%s/sample_corpus_2019-02-02" % project_path,
