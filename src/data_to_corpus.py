39aa0a35f2b4a4aa3c030d81828d391b6e9e98af
Rex Chen <rexcsn@umich.edu>
2019-02-20 21:57:00 -0500

    test

--- src/data_to_corpus.py
+++ src/data_to_corpus.py
@@ -9,8 +9,8 @@ import numpy as np
 import bz2
 import string
 
-# project_path = "/shared/0/projects/location-inference/working-dir/textual_data"
-project_path = "."
+project_path = "/shared/0/projects/location-inference/working-dir/textual_data"
+# project_path = "."
 
 
 def extract_info(filename):
@@ -28,9 +28,6 @@ def extract_info(filename):
     filtered = pd.DataFrame(
         columns=['user_id', 'tweet_id', 'text', 'lat', 'lon', 'city', 'country_code', 'source'])
 
-    print(df.iloc[0]['user'])
-    print(df.iloc[0]['text'])
-
     filtered.assign(user_id=df['user']['id'])
     filtered.assign(tweet_id=df['id'])
     filtered.assign(text=df['text'])
@@ -42,6 +39,7 @@ def extract_info(filename):
 
     filtered.to_csv(
         "%s/extracted_2019-02-02" % project_path)
+    print("Saved df to %s/extracted_2019-02-02 !!!" % project_path)
 
     return filtered
 
@@ -63,14 +61,15 @@ def to_corpus(df):
     dict_df.to_json(
         "%s/sample_corpus_2019-02-02" % project_path,
         orient='records')
+    print("Saved corpus to %s/sample_corpus_2019-02-02 !!!" % project_path)
 
     return dict_df
 
 
 def main():
-    # df = extract_info("/twitter-turbo/decahose/raw/decahose.2019-02-02.p1.bz2")
+    df = extract_info("/twitter-turbo/decahose/raw/decahose.2019-02-02.p1.bz2")
     # df = extract_info("./sample_text.txt.bz2")
-    df = extract_info("./baby.txt.bz2")
+    # df = extract_info("./baby.txt.bz2")
     to_corpus(df)
 
 
